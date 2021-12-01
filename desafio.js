b2049128d56cb810561131168b5ef6d83d367166
Flavio Andrade <foa12381@gmail.com>
2021-12-01 11:50:57 -0300

    task decode

--- desafio.js
+++ desafio.js
@@ -1,4 +1,27 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('');
 
-console.log(lines)
+// encriptografa
+let encodes =[]
+const encode = (c) => {
+  let b = c+12
+  encodes.push(b)
+}
+
+lines.forEach((c) => {
+  encode(c);
+})
+console.log("encode", encodes);
+
+// desencriptografar
+
+let decodes = [];
+const decode = (c) => {
+let a =c.split('');
+decodes.push(a[0]);
+}
+
+encodes.forEach((c) => {
+  decode(c)
+})
+console.log(decodes.join())
