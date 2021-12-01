a7181857cfbfb4797ec2a3a42f5550c3ab20c105
Flavio Andrade <foa12381@gmail.com>
2021-12-01 11:56:23 -0300

    task decode

--- desafio.js
+++ desafio.js
@@ -21,7 +21,12 @@ let a =c.split('');
 decodes.push(a[0]);
 }
 
+// if(!encode){console.log("nâo existe encriptografias ")};
+
 encodes.forEach((c) => {
   decode(c)
 })
-console.log(decodes.join())
+
+const result = decodes.join('');
+
+console.log(result);
