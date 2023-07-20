0e0fefbc5e82d5503435be8f3b36194e4992247f
Flavio Andrade <foa12381@gmail.com>
2023-07-20 10:04:39 -0300

    algoritimo calc

--- algotimo.js
+++ algotimo.js
@@ -1,1 +1,13 @@
-const sum = (a,b)=> a*b
+const calc = {
+  sum: sum = (a,b)=> a * b,
+  add: add = (a,b)=> a + b,
+  sub: sub = (a,b)=> a - b,
+  div: div = (a,b)=> a / b
+
+}
+const values = Object.keys(calc)
+console.log(values)
+
+const result = Math.floor(Math.random() * values.length)
+console.log(result, values[result])
+console.log(calc[values[result]](10,5));
--- moneyCounter.js
+++ moneyCounter.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
+var input = require('fs').readFileSync('data/stdin', 'utf8');
 var lines = input.split('\n');
 
 let valores = lines.shift();
