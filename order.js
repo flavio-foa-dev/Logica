193779e495b8de86595545f20c1f519e5cb4555d
Flavio Andrade <foa12381@gmail.com>
2023-07-21 22:04:58 -0300

    algoritimo

--- order.js
+++ order.js
@@ -9,13 +9,3 @@ for (let i = 0; i < price.length; i++) {
 }
 console.log(`o menor valor e =`, price[indexMenorValor])
 
-const price = [25, 15, 30, 50,45,20, 3]
-
-let indexMenorValor = 0
-
-for (let i = 0; i < price.length; i++) {
-  if (price[i] > price[indexMenorValor]) {
-    indexMenorValor = i
-  }
-}
-console.log(`o menor valor e =`, price[indexMenorValor])
