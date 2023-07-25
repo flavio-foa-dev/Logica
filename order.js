74d7dbc81d1568c4e6fb72863de95187bfb9cf8e
Flavio Andrade <foa12381@gmail.com>
2023-07-25 09:08:42 -0300

    feat/ordenar

--- order.js
+++ order.js
@@ -1,11 +1,28 @@
 const price = [25, 15, 30, 50,45,20, 3]
 
-let indexMenorValor = 0
 
-for (let i = 0; i < price.length; i++) {
-  if (price[i] > price[indexMenorValor]) {
-    indexMenorValor = i
+const lowerValue = (arr, index) => {
+  let indexMenorValor = index
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[indexMenorValor]) {
+      indexMenorValor = i
+    }
   }
-}
-console.log(`o menor valor e =`, price[indexMenorValor])
 
+  console.log(`o menor valor e =`, arr[indexMenorValor])
+}
+
+
+const biggerValue  = (arr, index) => {
+  let indexMenorValor = index
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[indexMenorValor]) {
+      indexMenorValor = i
+    }
+  }
+
+  console.log(`o maior valor e =`, arr[indexMenorValor])
+}
+
+lowerValue(price, 0)
+biggerValue(price, 0)
