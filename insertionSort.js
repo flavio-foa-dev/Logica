9baff48377d74a5e6dfeeeffe01c81d9681b9be5
Flavio Andrade <foa12381@gmail.com>
2023-07-25 09:54:49 -0300

    feat/ordenar

---
+++ insertionSort.js
@@ -0,0 +1,2 @@
+const data = require('./data/books.json')
+console.log(data)
--- order.js
+++ order.js
@@ -1,17 +1,17 @@
-const price = [25, 15, 30, 50,45,20, 3]
+let price = [25, 15, 30, 50,45,20, 3]
 
 
 const lowerValue = (arr, index) => {
   let indexMenorValor = index
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = indexMenorValor; i < arr.length; i++) {
     if (arr[i] < arr[indexMenorValor]) {
       indexMenorValor = i
     }
   }
 
-  console.log(`o menor valor e =`, arr[indexMenorValor])
-}
 
+  return indexMenorValor
+}
 
 const biggerValue  = (arr, index) => {
   let indexMenorValor = index
@@ -20,9 +20,21 @@ const biggerValue  = (arr, index) => {
       indexMenorValor = i
     }
   }
-
-  console.log(`o maior valor e =`, arr[indexMenorValor])
+  return arr[indexMenorValor]
 }
 
-lowerValue(price, 0)
-biggerValue(price, 0)
+console.log("Menor Valor", lowerValue(price, 0));
+console.log("Maior Valor", biggerValue(price, 0));
+console.log("array", price)
+
+price.forEach((_, indice) => {
+  let menor = lowerValue(price, indice)
+
+  let livroAtual = price[indice];
+  let livroMenorPreco = price[menor];
+
+  price[indice] = livroMenorPreco
+  price[menor] = livroAtual
+ })
+
+ console.log(price)
