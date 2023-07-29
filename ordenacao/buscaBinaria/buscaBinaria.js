6e5b63560de53e1151f317f14a8fbf985f8cdd09
Flavio Andrade <foa12381@gmail.com>
2023-07-29 15:55:56 -0300

    feat/binary

--- ordenacao/buscaBinaria/buscaBinaria.js
+++ ordenacao/buscaBinaria/buscaBinaria.js
@@ -1,2 +1,26 @@
 const data = require('../../data/booksOrdenado.json')
 // console.log(data)
+
+function getBinary(arr, de, ate, valor) {
+  const MEIO = Math.floor((de + ate) / 2);
+  const ATUAL = arr[MEIO]
+
+  if (de > ate) {
+    return -1
+  }
+
+  if(valor === ATUAL.preco) {
+    return MEIO // returna posicao
+    // return ATUAL // returna valor
+
+  }
+
+  if(valor < ATUAL.preco) {
+    return getBinary(arr, de, MEIO -1, valor)
+  }
+
+  if(valor > ATUAL.preco) {
+    return getBinary(arr, MEIO + 1, ate, valor)
+  }
+}
+console.log(getBinary(data, 0, data.length-1, 5))
