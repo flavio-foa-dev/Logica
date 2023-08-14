150860d0a3950efaa1da731f8a9a65cb635ff136
Flavio Andrade <foa12381@gmail.com>
2023-08-14 11:38:22 -0300

    menor-valor inteiro unico

---
+++ codility/menor-inteiro-positivo.js
@@ -0,0 +1,49 @@
+/**
+Escreva uma função:
+função solução(A);
+que, dada uma matriz A de N inteiros, retorna o menor inteiro positivo (maior que 0) que não ocorre em A.
+
+Por exemplo, dado A = [1, 3, 6, 4, 1, 2], a função deve retornar 5.
+Dado A = [1, 2, 3], a função deve retornar 4.
+Dado A = [−1, −3], a função deve retornar 1.
+Escreva um algoritmo eficiente para as seguintes suposições:
+N é um número inteiro dentro do intervalo [ 1 .. 100.000 ];
+cada elemento da matriz A é um inteiro dentro do intervalo [ −1.000.000 .. 1.000.000 ].
+**/
+
+// exemplo 1
+function solution(A) {
+  A.sort();
+  let menorNumber = 0
+  let menorNumeroUnico = 1
+  for(let i = 0; i < A.length; i ++ ){
+      if(A[menorNumber] > A[i]) {
+        menorNumber = i
+      }
+  }
+  for(let value of A){
+    if(menorNumeroUnico === value){
+      menorNumeroUnico += 1
+    }
+  }
+  return menorNumeroUnico < 1 ?  menorNumeroUnico = 1 : menorNumeroUnico
+}
+
+// exemplo 2
+function solutionn(A) {
+  A.sort();
+  let menorNumeroUnico = A[0]
+  for(let value of A){
+    if(menorNumeroUnico === value){
+      menorNumeroUnico += 1
+    }
+  }
+  return menorNumeroUnico < 1 ?  menorNumeroUnico = 1 : menorNumeroUnico
+}
+
+ console.log("solucao",solution([-1, -3]))
+ console.log("solucao",solution([1, 3, 6, 4, 1, 2]))
+ console.log("solucao" ,solution([1, 2, 3]))
+
+
+
